20a3d391304d51f4b89ac94c2ef1f18558596ea5
awesomelisabeth-beep <awesomelisabeth@gmail.com>
2026-04-10 10:10:59 -0400

    added navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,3 +1,4 @@
 $(function(){
+    console.log("Hi")
     $("#nav-placeholder").load("navbar.html")
 })
---
+++ projects/my.js
@@ -0,0 +1,4 @@
+$(function(){
+    console.log("Hi")
+    $("#nav-placeholder").load("./../navbar.html")
+})
